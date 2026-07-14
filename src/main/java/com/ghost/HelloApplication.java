eb8093bb489ffb1665607ab23572968d34af6791
GhostC <yueyehack@gmail.com>
2026-07-14 11:30:49 +0800

    Update HelloApplication.java

--- src/main/java/com/ghost/HelloApplication.java
+++ src/main/java/com/ghost/HelloApplication.java
@@ -1,9 +1,13 @@
 package com.ghost;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HelloApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello GitHub Actions!");
+        SpringApplication.run(HelloApplication.class, args);
     }
 
 }
